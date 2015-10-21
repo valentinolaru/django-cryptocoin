938a27c2baaadc5c82ca2f1db6026925742c72c8
Valentin Olaru <vali@pidginhost.com>
2015-10-21 17:14:19 +0300

    get exchange rates updated with module requests

--- django_cryptocoin/management/commands/get_exchange_rates.py
+++ django_cryptocoin/management/commands/get_exchange_rates.py
@@ -1,5 +1,5 @@
-import httplib
-import json
+import requests
+
 from django.core.management.base import BaseCommand
 from django_cryptocoin.models import ExchangeRate
 from django_cryptocoin import settings
@@ -13,12 +13,15 @@ class Command(BaseCommand):
 
         for pair in settings.CURRENCY_PAIRS:
             currencies = pair.split('_')
-            conn = httplib.HTTPSConnection("btc-e.com")
-            conn.request("GET", "/api/2/%s/ticker" % pair)
-            response = conn.getresponse()
-            rate, created = ExchangeRate.objects.get_or_create(currency1=currencies[0], currency2=currencies[1])
-            json_resp = json.load(response)
-            rate.rate = json_resp['ticker']['last']
-            rate.save()
+            try:
+                response = requests.get('http://btc-e.com/api/2/{}/ticker'.format(pair)).json()
+            except Exception as e:
+                self.stdout.write('Get error: {}'.format(e))
+                response = None
+
+            if response:
+                rate, created = ExchangeRate.objects.get_or_create(currency1=currencies[0], currency2=currencies[1])
+                rate.rate = response['ticker']['last']
+                rate.save()
 
         self.stdout.write('Successfully executed')
