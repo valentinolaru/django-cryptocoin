91d6a4021eaaa350307bd6aa8a8bd2db64b32f6a
Valentin Olaru <vali@pidginhost.com>
2015-10-21 14:25:23 +0300

    python3 fixes

--- django_cryptocoin/models.py
+++ django_cryptocoin/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-from bitcoinrpc.authproxy import AuthServiceProxy
+from django_cryptocoin.bitcoinrpc.authproxy import AuthServiceProxy
 from django_cryptocoin import settings
 
 
