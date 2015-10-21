e13f2561cc07a2152ae071ab8c1e5d9075e74068
Valentin Olaru <vali@pidginhost.com>
2015-10-21 15:00:48 +0300

    fix currency migrations continually

--- django_cryptocoin/settings.py
+++ django_cryptocoin/settings.py
@@ -3,45 +3,45 @@ from django.conf import settings
 CRYPTO_COINS = getattr(
     settings,
     "CRYPTO_COINS",
-    {
-        'btc': 'Bitcoin',
-        'ltc': 'Litecoin',
-        'nvc': 'Novacoin',
-        'emc': 'Emercoin',
-    }
+    (
+        ('btc', 'Bitcoin'),
+        ('ltc', 'Litecoin'),
+        ('nvc', 'Novacoin'),
+        ('emc', 'Emercoin')
+    )
 )
 
 CONNECTION_STRING = getattr(
     settings,
     "CONNECTION_STRING",
-    {
-        'btc': 'http://user:pass@localhost:8332',
-        'ltc': 'http://user:pass@localhost:9332',
-        'nvc': 'http://user:pass@localhost:8344',
-        'emc': 'http://user:pass@localhost:8100',
-    }
+    (
+        ('btc', 'http://user:pass@localhost:8332'),
+        ('ltc', 'http://user:pass@localhost:9332'),
+        ('nvc', 'http://user:pass@localhost:8344'),
+        ('emc', 'http://user:pass@localhost:8100')
+    )
 )
 
 MAIN_ADDR = getattr(
     settings,
     "MAIN_ADDR",
-    {
-        'btc': 'your_bitcoin_addr_to_send_btc',
-        'ltc': 'your_litecoin_addr_to_send_ltc',
-        'nvc': 'your_novacoin_addr_to_send_nvc',
-        'emc': 'your_emercoin_addr_to_send_emc',
-    }
+    (
+        ('btc', 'your_bitcoin_addr_to_send_btc'),
+        ('ltc', 'your_litecoin_addr_to_send_ltc'),
+        ('nvc', 'your_novacoin_addr_to_send_nvc'),
+        ('emc', 'your_emercoin_addr_to_send_emc')
+    )
 )
 
 CONFIRMATIONS = getattr(
     settings,
     "CONFIRMATIONS",
-    {
-        'btc': 1,
-        'ltc': 1,
-        'nvc': 1,
-        'emc': 1,
-    }
+    (
+        ('btc', 1),
+        ('ltc', 1),
+        ('nvc', 1),
+        ('emc', 1)
+    )
 )
 
 PROCESS_TEMPLATE = getattr(
@@ -62,9 +62,7 @@ GENERATED_ADDRESSES_ACCOUNT = getattr(
     'django_cryptocoin'
 )
 
-
-
-#currency pairs for which will retrieved exchange rate from btc-e
+# Currency pairs for which will retrieved exchange rate from btc-e
 CURRENCY_PAIRS = getattr(
     settings,
     "CURRENCY_PAIRS",
